a74b1218f54c941c5f473090138f6f43e2b3a679
11juan22 <56978943+11juan22@users.noreply.github.com>
2021-02-25 18:35:10 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-cars',{delay:500});
+ScrollReveal().reveal('.cars-banner-one',{delay:500});
+ScrollReveal().reveal('.cars-banner-two',{delay:500});
+
+
+document.querySelector('.menu-btn').addEventListener('click',() => {
+    document.querySelector('.nav-menu').classList.toggle('show')
+
+});
